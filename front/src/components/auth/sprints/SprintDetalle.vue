4801419dcd12477a568d0f3ff910f870be0ea9a5
Marcelo López <marcelolopezhernandez@gmail.com>
2021-04-17 16:57:43 -0400

    añade métodos en backend para extraer informacion de sprint (pathvariable request)

--- front/src/components/auth/sprints/SprintDetalle.vue
+++ front/src/components/auth/sprints/SprintDetalle.vue
@@ -4,20 +4,33 @@
 
 
 <script>
-//import axios from "axios";
+import axios from "axios";
 export default {
   name: "SprintDetalle",
 
   data() {
     return {
-      
+      apiUrl: "http://localhost:8080/",
+      sprint: null,
     };
   },
   mounted() {
-    console.log("IN SPRINT")
+    this.id = this.$route.params.sprint;
+    this.getInfo();
   },
   methods: {
-   
+    async getInfo() {
+      let token = localStorage.getItem("token");
+      await axios
+        .get(this.apiUrl + "getInfoSprint/" + this.id, {
+          headers: {
+            Authorization: token,
+          },
+        })
+        .then((response) => (this.sprint = response.data))
+        .catch((error) => console.log(error));
+      console.log(this.sprint);
+    },
   },
 };
 </script>
