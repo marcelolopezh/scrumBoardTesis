f2bebe1e4707927dccaed0ac0765e3d1b2f69497
Marcelo Lopez <marcelolopezhernandez@gmail.com>
2021-05-05 02:17:29 -0400

    Fix KanBan

--- front/src/components/auth/sprints/SprintDetalle.vue
+++ front/src/components/auth/sprints/SprintDetalle.vue
@@ -35,8 +35,10 @@
                           class="elevation-1"
                           @page-count="pageCount = $event"
                           show-expand
-                          right
                         >
+                          <template v-slot:[`item.name`]="{ item }">
+                            <strong>{{ item.name }}</strong>
+                          </template>
                           <template v-slot:[`item.state`]="{ item }">
                             <v-chip
                               v-if="item.state == 'Pendiente'"
@@ -250,19 +252,22 @@
                             elevation="2"
                           >
                             <div v-for="item in sprint.tasks" :key="item.id">
-                              <v-list v-if="item.state == 'Pendiente'" dense>
+                              <v-list disabled v-if="item.state == 'Pendiente'">
                                 <v-list-item-group>
                                   <v-list-item>
                                     <v-list-item-content>
                                       <v-list-item-title
-                                        v-text="item.name"
-                                      ></v-list-item-title>
+                                        ><strong>{{
+                                          item.name
+                                        }}</strong></v-list-item-title
+                                      >
                                     </v-list-item-content>
                                   </v-list-item>
                                 </v-list-item-group>
                               </v-list>
-                            </div> </v-alert
-                        ></v-card-text>
+                            </div>
+                          </v-alert></v-card-text
+                        >
                       </v-card>
                     </v-col>
                     <v-col cols="4">
@@ -276,19 +281,20 @@
                             elevation="2"
                           >
                             <div v-for="item in sprint.tasks" :key="item.id">
-                              <v-list v-if="item.state == 'En Curso'" dense>
+                              <v-list disabled v-if="item.state == 'En Curso'">
                                 <v-list-item-group>
                                   <v-list-item>
                                     <v-list-item-content>
-                                      <v-list-item-title
-                                        v-text="item.name"
-                                      ></v-list-item-title>
+                                      <v-list-item-title>
+                                        <strong>{{ item.name }}</strong>
+                                      </v-list-item-title>
                                     </v-list-item-content>
                                   </v-list-item>
                                 </v-list-item-group>
                               </v-list>
                             </div></v-alert
-                        ></v-card-text>
+                          ></v-card-text
+                        >
                       </v-card> </v-col
                     ><v-col cols="4">
                       <v-card>
@@ -300,20 +306,23 @@
                             color="success"
                             elevation="2"
                           >
-                           <div v-for="item in sprint.tasks" :key="item.id">
-                              <v-list v-if="item.state == 'Terminado'" dense>
+                            <div v-for="item in sprint.tasks" :key="item.id">
+                              <v-list disabled v-if="item.state == 'Terminado'">
                                 <v-list-item-group>
                                   <v-list-item>
                                     <v-list-item-content>
                                       <v-list-item-title
-                                        v-text="item.name"
-                                      ></v-list-item-title>
+                                        ><strong>{{
+                                          item.name
+                                        }}</strong></v-list-item-title
+                                      >
                                     </v-list-item-content>
                                   </v-list-item>
                                 </v-list-item-group>
                               </v-list>
-                            </div> </v-alert
-                        ></v-card-text>
+                            </div>
+                          </v-alert></v-card-text
+                        >
                       </v-card>
                     </v-col>
                   </v-row>
